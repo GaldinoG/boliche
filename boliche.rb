5c35a7f157adbfbc4a6ea9098edc4a07275db12e
André Alves Rutter Salles <1000575@PPC01INFLL00318.pucminas.net>
2019-04-29 19:31:58 -0300

    PontuacaoUsuario

--- boliche.rb
+++ boliche.rb
@@ -39,5 +39,154 @@ class PontuacaoNivelPC
     	end
 end
 
+# Classe responsável pelos calculos do jogo
+# @author Gabriel Galdino, André Alves
+class PontuacaoUsuario
+
+	# Construtor
+	# @return [ptos] retorna o total do Score
+	def calculaPontuacao(nome)
+		
+		qt_pinos, qt_pinos2, ptos, i, j = [0,0,0,1,1]
+		booStrike = false
+		sumProx = false
+		auxValor2 = false
+		auxSpare = false
+		strAux = 0
+		countSoma = Array.new
+		contagem = 10
+
+		for i in 1..contagem
+			loop do
+				puts("\n#{nome} quantos pinos voce derrubou no frame #{i} da jogada 1: ")
+				qt_pinos = gets.chomp.to_i
+				system("clear")
+				break if qt_pinos <= 10
+			end
+				
+			if auxValor2 == true or auxSpare == true
+				ptos = ptos + qt_pinos
+				auxSpare = false
+			end
+
+			if sumProx == true 
+				ptos = ptos + qt_pinos
+				sumProx = false
+				auxValor2 = true
+			end
+			
+			if qt_pinos < 10
+				loop do
+					puts("#{nome} quantos pinos voce derrubou no frame #{i} da jogada 2: ")
+					qt_pinos2 = gets.chomp.to_i
+					system("clear")
+					break if qt_pinos + qt_pinos2 <= 10
+				end
+
+				if auxValor2 == true 
+					ptos = ptos + qt_pinos2
+					auxValor2 = false
+				end
+
+				if qt_pinos + qt_pinos2 == 10
+					auxSpare = true
+				end 
+				
+			elsif qt_pinos == 10
+				if i == contagem then
+					booStrike = true
+				end
+				sumProx = true 
+				auxSpare = false
+			end
+
+			system("clear")
+
+			sum = qt_pinos + qt_pinos2
+        		ptos=ptos+sum
+			
+			if sumProx == true 
+				countSoma.push("Jogada #{i} --> Pontos #{sum} --> Strike")
+			elsif auxSpare == true
+				countSoma.push("Jogada #{i} --> Pontos #{sum} --> Spare")
+			else
+				countSoma.push("Jogada #{i} --> Pontos #{sum}")
+			end
+
+
+        	puts ptos
+        		
+        	qt_pinos = 0
+        	qt_pinos2 = 0
+		end
+
+		if booStrike == true or auxSpare == true
+			
+			if sumProx == true 
+				bonus = 1
+				puts("\n\nVoce ganhou 2 jogadas bonus pelo strike feito no primeiro arremeco do ultimo frame!!!")
+			elsif auxSpare == true
+				bonus = 2
+				puts("\n\nVoce ganhou 1 jogada bonus pelo spare feito no ultimo frame!!!")
+			end
+			
+			aux = bonus
+			for var in aux..2
+				
+				loop do
+					puts("\n\n#{nome} quantos pinos voce derrubou na jogada Bonus de frame 1?")
+					qt_pinos = gets.chomp.to_i
+					system("clear")
+					break if qt_pinos <= 10
+				end
+				
+				if auxSpare == true
+					ptos = ptos + qt_pinos
+					auxSpare = false
+				elsif sumProx == true 
+					ptos = ptos + qt_pinos
+					sumProx = false
+					auxValor2 = true
+				end
+				
+				if bonus == 1 
+					if qt_pinos < 10
+						loop do
+							puts("#{nome} quantos pinos voce derrubou na jogada Bonus de frame #{bonus} da jogada 2: ")
+							qt_pinos2 = gets.chomp.to_i
+							system("clear")
+							break if qt_pinos + qt_pinos2 <= 10
+						end
+					
+						if auxValor2 == true 
+							ptos = ptos + qt_pinos2
+							auxValor2 = false
+						end
+
+						if qt_pinos + qt_pinos2 == 10
+							auxSpare = true
+						end 
+					elsif qt_pinos == 10
+						auxValor2 = false
+					end
+				end
+
+			system("clear")
+
+			sum = qt_pinos + qt_pinos2
+        		ptos=ptos+sum
+        		puts "#{ptos}\n"
+        		
+        		qt_pinos = 0
+        		qt_pinos2 = 0
+			end
+		end 
+	countSoma.each { |a| a }
+	puts countSoma
+	return ptos
+	end
+end
+
+
 nme = SetaJogador.new
 nome = nme.cadastro
